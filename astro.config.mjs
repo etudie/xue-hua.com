20651afc1c33b2818af06eff2e98838c688e2190
Xue <xue.contact@gmail.com>
2026-02-16 22:48:29 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-  integrations: [sanity(), react()]
+  integrations: [sanity(), react(), sitemap()]
 });
