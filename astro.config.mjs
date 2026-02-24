d8b591805a8b7a5b58ee922323846e404724161d
Xue <xue.contact@gmail.com>
2026-02-23 21:46:50 -0500

    Install DaisyUI

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  integrations: [sanity(), react(), sitemap()]
+  integrations: [sanity(), react(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
