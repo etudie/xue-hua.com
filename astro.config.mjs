e223bec2b49eba3d0a8271be5c2fe6768677b53e
Xue <xue.contact@gmail.com>
2026-02-25 01:24:26 -0500

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
-import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [sanity(), react(), sitemap()],
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
