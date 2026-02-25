7a4d2cf801d17e9991947802b3164320cb8ac43d
Xue <xue.contact@gmail.com>
2026-02-25 01:25:18 -0500

    Fonts, styles, tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+        serif: ["Georgia", "serif"],
+        mono: ["Fira Code", "monospace"],
+      },
+    },
+  },
+};
