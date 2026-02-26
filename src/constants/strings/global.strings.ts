44a69754ec1338ba8b74ba37c94e7bdfc5d4db63
Xue <xue.contact@gmail.com>
2026-02-25 21:04:21 -0500

    Aligned footer text center

---
+++ src/constants/strings/global.strings.ts
@@ -0,0 +1,10 @@
+export const GLOBAL = {
+  site: {
+    author: "Xue Hua",
+    xuehua: "Xue Hua",
+    title: "Xue Hua",
+    llc: "Lantern LLC",
+    state: "Michigan",
+    city: "Grand Rapids",
+  },
+};
