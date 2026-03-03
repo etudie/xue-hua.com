7f80ac6218b61f809737e1130523ebfde4e35f0c
Xue <xue.contact@gmail.com>
2026-03-03 12:12:23 -0500

    Add Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   integrations: [react(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
